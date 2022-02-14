3e103e5f649156ef544f8a04b437f72d0cf2b6fb
luchik21 <luchezavr640595@gmail.com>
2022-02-14 13:36:17 +0200

    Initial commit

---
+++ src/main/java/com/lab3/zalotov/laba3/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.lab3.zalotov.laba3;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(Laba3Application.class);
+    }
+
+}
